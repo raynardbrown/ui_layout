edde418bdc68edee27edd3287ba37aea4bba4fa6
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-11-12 23:59:03 -0600

    feat: Add RowLayout header

---
+++ inc/RowLayout.h
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: RowLayout.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2019 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ROWLAYOUT_H_
+#define ROWLAYOUT_H_
+
+class RowLayout
+{
+  public:
+    RowLayout();
+    virtual ~RowLayout();
+};
+#endif /* ROWLAYOUT_H_ */
