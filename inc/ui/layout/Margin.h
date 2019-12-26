2f6b3c2215da9a845f011013ae2d782c4b18e493
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-12-25 23:42:16 -0600

    feat: Add the Margin type

---
+++ inc/ui/layout/Margin.h
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: Margin.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2019 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef MARGIN_H_
+#define MARGIN_H_
+
+struct Margin
+{
+    int leftMargin;
+    int rightMargin;
+    int topMargin;
+    int bottomMargin;
+};
+#endif /* MARGIN_H_ */
