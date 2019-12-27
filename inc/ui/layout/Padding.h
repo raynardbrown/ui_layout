5840d6465a2eba86ec3f454a79598f2e58b1ac0a
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2019-12-26 22:54:52 -0600

    feat: Add the Padding type

---
+++ inc/ui/layout/Padding.h
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: Padding.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2019 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef PADDING_H_
+#define PADDING_H_
+
+struct Padding
+{
+    int leftPadding;
+    int rightPadding;
+    int topPadding;
+    int bottomPadding;
+};
+#endif /* PADDING_H_ */
