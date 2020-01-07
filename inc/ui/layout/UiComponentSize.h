736ec3cfbf6789fc42141724fedc62f06810fc51
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-01-07 02:41:33 -0600

    feat: Add the UiComponentSize header

---
+++ inc/ui/layout/UiComponentSize.h
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: UiComponentSize.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef UI_LAYOUT_UICOMPONENTSIZE_H_
+#define UI_LAYOUT_UICOMPONENTSIZE_H_
+
+class UiComponentSize
+{
+  public:
+
+    UiComponentSize();
+
+    ~UiComponentSize();
+
+    static int getButtonWidth();
+
+    static int getButtonHeight();
+};
+#endif /* UI_LAYOUT_UICOMPONENTSIZE_H_ */
