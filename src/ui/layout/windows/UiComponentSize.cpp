ef8bf1b88813d0e0eeed05737423938411c1f655
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-01-07 02:46:00 -0600

    feat: Add the UiComponent source

---
+++ src/ui/layout/windows/UiComponentSize.cpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: UiComponentSize.cpp
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "ui/layout/UiComponentSize.h"
+
+UiComponentSize::UiComponentSize()
+{
+}
+
+UiComponentSize::~UiComponentSize()
+{
+}
+
+/*static*/ int UiComponentSize::getButtonWidth()
+{
+  return 75;
+}
+
+/*static*/ int UiComponentSize::getButtonHeight()
+{
+  return 23;
+}
