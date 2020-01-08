3287a2945f57452c3c9283ec990dcbc0af56ab68
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-01-07 23:47:43 -0600

    feat: Add the LabelOrientation enum

---
+++ inc/ui/layout/LabelOrientation.h
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: LabelOrientation.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef UI_LAYOUT_LABELORIENTATION_H_
+#define UI_LAYOUT_LABELORIENTATION_H_
+
+struct LabelOrientation
+{
+  public:
+
+    enum Value
+    {
+      LeftOrientation,
+      RightOrientation,
+      TopOrientation,
+      BottomOrientation
+    };
+};
+#endif /* UI_LAYOUT_LABELORIENTATION_H_ */
