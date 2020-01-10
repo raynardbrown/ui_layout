59351d531f0c8c4b5c2cff0d98bb590c48183f67
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-01-09 23:01:17 -0600

    feat: Add the UiComponentWrapper header

---
+++ src/ui/layout/private/UiComponentWrapper.h
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: UiComponentWrapper.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef UI_LAYOUT_PRIVATE_UICOMPONENTWRAPPER_H_
+#define UI_LAYOUT_PRIVATE_UICOMPONENTWRAPPER_H_
+
+class UiComponent;
+class Label;
+class UiComponentConstraints;
+
+struct UiComponentWrapper
+{
+    UiComponent * uiComponent;
+    Label * labelPeer;
+    UiComponentConstraints * uiComponentConstraints;
+};
+#endif /* UI_LAYOUT_PRIVATE_UICOMPONENTWRAPPER_H_ */
