66cb32ea7ea1560a05b791cecbdb71a2749a23cc
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-01-12 22:28:09 -0600

    feat: Add the getLabelHeight API

--- src/ui/layout/windows/UiComponentSize.cpp
+++ src/ui/layout/windows/UiComponentSize.cpp
@@ -29,3 +29,19 @@ UiComponentSize::~UiComponentSize()
 {
   return 23;
 }
+
+/*static*/ int UiComponentSize::getLabelHeight()
+{
+  /*
+   * According to the UXGuide.pdf, Text (static) controls, in my case Labels,
+   * are recommended to have a height of 13 pixels (Given a Segoe UI 9 pt font).
+   * However when testing I have discovered clipping in the descent. I also
+   * discovered that this particular recommendation works with the font,
+   * MS Shell Dlg 2 8pt font.
+   *
+   * With further testing, I have discovered that the correct Label height for
+   * the Segoe UI 9pt font is 15. Also the tmHeight field of the TEXTMETRIC
+   * structure confirms this when the font is selected into the device context.
+   */
+  return 15;
+}
