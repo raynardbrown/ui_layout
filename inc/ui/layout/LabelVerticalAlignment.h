29719f73fcfea5c4a6dc0c32c94e9bcd16f9ce70
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-01-14 23:49:31 -0600

    feat: Add the LabelVerticalAlignment header

---
+++ inc/ui/layout/LabelVerticalAlignment.h
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: LabelVerticalAlignment.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef UI_LAYOUT_LABELALIGNMENT_H_
+#define UI_LAYOUT_LABELALIGNMENT_H_
+
+struct LabelVerticalAlignment
+{
+  public:
+
+    enum Value
+    {
+      TopAlignment,
+      MiddleAlignment,
+      BottomAlignment
+    };
+};
+#endif /* UI_LAYOUT_LABELALIGNMENT_H_ */
